bf9417c55d1a41d97470c36b07602fc11be46099
Jenifer Ananth <ananthfrancis>
2021-02-25 12:03:07 +0800

    new code

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,10 @@ router.get('/healthcheck', function(req, res, next) {
   res.sendStatus(200)
 });
 
+router.get('/envCheck', function(req,res, next){
+  let env_name=process.env.env_name || "test";
+  console.log(env_name)
+  res.send(env_name);
+});
+
 module.exports = router;
